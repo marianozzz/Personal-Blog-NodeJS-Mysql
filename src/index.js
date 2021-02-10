4d9249273e4d28411ff50c10f979622abfaf1cfd
marianozzz <mariano.zanier@gmail.com>
2021-02-09 21:20:18 -0300

    hola mundo

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.set('views',path.join(__dirname, 'views'));
 app.engine('ejs',engine);
 app.set('view engine', 'ejs');
 
+
 // middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
